397a25cd00cf4103c22ca4f6839006440fbf0b07
vijay-r-a-a-j <vijayteja648@gmail.com>
2024-10-04 15:55:45 +0530

    Created the E-learn project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen bg-rose-100 text-5xl text-center my-auto'>Welocme to E-learn app</div>
+  )
+}
+
+export default App
